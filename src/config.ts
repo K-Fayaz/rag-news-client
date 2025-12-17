21cc646540b324b1895f6d33a36b07e17c2981e5
Fayaz <kfayaz1407@gmail.com>
2025-12-17 17:42:56 +0530

    updated config base url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 const DEV_BASE = "http://localhost:8080";
 const PROD_BASE = "";
+const RENDER_BASE = "https://rag-news-backend-tlr8.onrender.com";
 
-const BASE_URL = PROD_BASE;
+const BASE_URL = RENDER_BASE;
 export default BASE_URL;
